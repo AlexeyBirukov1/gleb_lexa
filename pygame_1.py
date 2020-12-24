8cc7f30159dd86b96bb3171c8af51eb72b213e35
AlexeyBirukov1 <62579872+AlexeyBirukov1@users.noreply.github.com>
2020-12-24 23:23:17 +0700

    фиксим

--- pygame_1.py
+++ pygame_1.py
@@ -17,7 +17,7 @@ class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
+        self.bg = pygame.image.load("sp/backgroung.png")
         self.pushButton_3.clicked.connect(self.select)
 
     def select(self):
@@ -60,70 +60,10 @@ class MyWidget(QMainWindow, Ui_MainWindow):
                         sprite.rect.x = sprite.rect.x + 2
                     if key[pygame.K_LEFT]:
                         sprite.rect.x = sprite.rect.x - 2
-                    screen.fill(pygame.Color(WHITE))
+                    screen.blit(self.bg, (0, 0))
                     all_sprites.draw(screen)
                     clock.tick(fps)
                     pygame.display.flip()
-                # while running:
-                #     if y_velocity < gy:
-                #         x_velocity += 5
-                #         y_velocity += 5
-                #     if x_velocity == 600:
-                #         x_velocity = 300
-                #         y_velocity = 300
-                #     if x_velocity == 0:
-                #         x_velocity = 300
-                #         y_velocity = 300
-                #     if x_velocity + 7.5 == sp2 and y_velocity < 300:
-                #         x_velocity -= 5
-                #         y_velocity += 5
-                #     if x_velocity + 7.5 == sp2 and y_velocity > 300:
-                #         x_velocity -= 5
-                #         y_velocity -= 5
-                #     if x_velocity == sp1 and y_velocity < 300:
-                #         x_velocity += 5
-                #         y_velocity += 5
-                #     if x_velocity == sp1 and y_velocity > 300:
-                #         x_velocity += 5
-                #         y_velocity -= 5
-                #
-                #     keys = pygame.key.get_pressed()
-                #
-                #     if keys[pygame.K_w] and y_movement1 > 25:
-                #         y_movement1 -= 1.8
-                #     if keys[pygame.K_s] and y_movement1 < 575 - 25 - 6:
-                #         y_movement1 += 1.8
-                #     if keys[pygame.K_UP] and y_movement2 > 25:
-                #         y_movement2 -= 1.8
-                #     if keys[pygame.K_DOWN] and y_movement2 < 550 - 25 - 6:
-                #         y_movement2 += 1.8
-                #
-                #
-                #     speed.append([1, -0.5])
-                #     for event in pygame.event.get():
-                #         # при закрытии окна
-                #         if event.type == pygame.QUIT:
-                #             running = False
-                #         if event.type == pygame.KEYUP:
-                #             if event.key == pygame.K_ESCAPE:
-                #                 running = False
-                #     screen2.fill(WHITE)
-                #     screen.fill(WHITE)
-                #     for i in range(len(circles)):
-                #         for x in (0, 1):
-                #             if circles[i][x] + r >= size[x] or circles[i][x] - r <= 0:
-                #                 speed[i][x] = -speed[i][x]
-                #
-                #             circles[i][x] += speed[i][x]
-                #         pygame.draw.circle(screen2, (ORANGE), circles[i], r)
-                #         pygame.draw.rect(screen, (PINK), (sp1, y_movement1, 10, 100))
-                #         pygame.draw.rect(screen, (ORANGE), (sp2, y_movement2, 10, 100))
-                #     screen.blit(screen2, (0, 0))
-                #
-                #     # обновление экрана
-                #     pygame.display.flip()
-                #     fps = 200
-                #     clock.tick(fps)
 
                 pygame.quit()
         if __name__ == "__main__":
