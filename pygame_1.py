11b8faa1ba3cb3772464471d5a96fb1a1381585c
AlexeyBirukov1 <62579872+AlexeyBirukov1@users.noreply.github.com>
2020-12-25 23:26:37 +0700

    ТЕСТИМ НОВОЕ МЕНЮ

--- pygame_1.py
+++ pygame_1.py
@@ -23,67 +23,74 @@ all_sprites.add(sprite)
 all_sprites2.add(sprite2)
 
 
-class MyWidget(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.bg = pygame.image.load("sp/backgroung.png")
-        self.pushButton_3.clicked.connect(self.select)
+bg = pygame.image.load("sp/backgroung.png")
+bg2 = pygame.image.load("sp/menu.png")
 
-    def select(self):
-        def main():
-            circles = []
-            if __name__ == '__main__':
-                pygame.init()
-                pygame.display.set_caption('ШАРЫ')
+def menu():
+    circles = []
+    if __name__ == '__main__':
+        pygame.init()
+        pygame.display.set_caption('ШАРЫ')
+        screen = pygame.display.set_mode((600, 600))
+        running = True
+        clock = pygame.time.Clock()
+        fps = 100
 
-                screen = pygame.display.set_mode((800, 600))
-                circles.append(list((400, 300)))
-                running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    running = False
+            screen.blit(bg2, (0, 0))
+            clock.tick(fps)
+            pygame.display.flip()
+        main()
 
-                clock = pygame.time.Clock()
-                fps = 100
+def main():
+    circles = []
+    if __name__ == '__main__':
+        pygame.init()
+        pygame.display.set_caption('ШАРЫ')
+        screen = pygame.display.set_mode((800, 600))
+        circles.append(list((400, 300)))
+        running = True
+        clock = pygame.time.Clock()
+        fps = 100
+        sprite.rect.y = 520
+        sprite2.rect.y = 300
+        sprite2.rect.x = 400
 
-                sprite.rect.y = 520
-                sprite2.rect.y = 300
-                sprite2.rect.x = 400
+        dx = 2
+        dy = 2
 
-                dx = 2
-                dy = 2
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            key = pygame.key.get_pressed()
+            sprite2.rect.x += dx
+            sprite2.rect.y += dy
+            if sprite2.rect.y > 525 or sprite2.rect.y < 0:
+                dy *= -1
+            if sprite2.rect.x > 725 or sprite2.rect.x < 0:
+                dx *= -1
+            if sprite2.rect.x == sprite.rect.x and sprite2.rect.y == 550:
+                dy *= -1
+            # ------------------------------------------
+            if key[pygame.K_RIGHT]:
+                sprite.rect.x = sprite.rect.x + 2
+            if key[pygame.K_LEFT]:
+                sprite.rect.x = sprite.rect.x - 2
+            screen.blit(bg, (0, 0))
+            try:
+                all_sprites.draw(screen)
+                all_sprites2.draw(screen)
+            except Exception as e:
+                print(e)
+            clock.tick(fps)
+            pygame.display.flip()
 
-                while running:
-                    for event in pygame.event.get():
-                        if event.type == pygame.QUIT:
-                            running = False
-                    key = pygame.key.get_pressed()
-                    sprite2.rect.x += dx
-                    sprite2.rect.y += dy
-                    if sprite2.rect.y > 525 or sprite2.rect.y < 0:
-                        dy *= -1
-                    if sprite2.rect.x > 725 or sprite2.rect.x < 0:
-                        dx *= -1
-                    if sprite2.rect.x == sprite.rect.x and sprite2.rect.y == 550:
-                        dy *= -1
-                    # ------------------------------------------
-                    if key[pygame.K_RIGHT]:
-                        sprite.rect.x = sprite.rect.x + 2
-                    if key[pygame.K_LEFT]:
-                        sprite.rect.x = sprite.rect.x - 2
-                    screen.blit(self.bg, (0, 0))
-                    try:
-                        all_sprites.draw(screen)
-                        all_sprites2.draw(screen)
-                    except Exception as e:
-                        print(e)
-                    clock.tick(fps)
-                    pygame.display.flip()
-
-                pygame.quit()
-        if __name__ == "__main__":
-            main()
+        pygame.quit()
 
 
-app = QApplication(sys.argv)
-ex = MyWidget()
-ex.show()
-sys.exit(app.exec_())
+if __name__ == "__main__":
+    menu()
